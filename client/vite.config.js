d1c062cb60689d270161ce21ea55529b89aee1ce
Nati Darse <natnaeldarsema@gmail.com>
2026-01-17 05:34:57 +0300

    optimized your build and prepared the project for global deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,6 +16,22 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-mui-core': ['@mui/material', '@emotion/react', '@emotion/styled'],
+          'vendor-mui-icons': ['@mui/icons-material'],
+          'vendor-mui-grid': ['@mui/x-data-grid'],
+          'vendor-mui-lab': ['@mui/lab'],
+          'vendor-charts': ['recharts', 'chart.js', 'react-chartjs-2'],
+          'vendor-react': ['react', 'react-dom', 'react-router-dom', '@tanstack/react-query'],
+          'vendor-utils': ['axios', 'date-fns', 'socket.io-client', 'formik', 'yup', 'lucide-react']
+        }
+      }
+    }
+  },
   optimizeDeps: {
     include: ['@emotion/react', '@emotion/styled'],
   },
